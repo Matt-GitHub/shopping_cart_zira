712199ce08e5dffec4540e6297a0f906cff6c7f2
Matt-GitHub <matthewjamesbedard@outlook.com>
2021-08-26 08:43:27 -0400

    init cleanup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import productReducer from "../features/products/productsSlice";
 import createReducer from "../features/cart/cartSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     products: productReducer,
     cart: createReducer
   }
